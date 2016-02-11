a3076ea5e2f9e0a3f1be60a2613e07132e3684be
Yunfei Hao <yunfei.hao@intel.com>
2016-02-12 00:43:52 +0800

    Change the serverfile name as implementation.

--- ocf-servers/init-ocf-server.sh
+++ ocf-servers/init-ocf-server.sh
@@ -16,14 +16,19 @@ if [[ $? -ne 0 ]];then
   systemctl start iot-rest-api-server
 else
   echo Rest API Server was launched
-
 fi
 
-cd ocf-servers/js-servers
-export NODE_DEBUG=led1; node led1.js &
-export NODE_DEBUG=led2; node led2.js &
-export NODE_DEBUG=led3; node led3.js &
-export NODE_DEBUG=led4; node led4.js &
-export NODE_DEBUG=buzzer; node buzzer.js &
-export NODE_DEBUG=rgb_led; node rgb_led.js &
+currentPath=$(cd `dirname $0`; pwd)
+runJs() {
+  ps | grep $1.js | grep -v grep
+  if [[ $? -ne 0 ]];then
+    export NODE_DEBUG=$1; node $currentPath/$1.js &
+  fi
+}
 
+runJs rgb_led1
+runJs rgb_led2
+runJs rgb_led3
+runJs rgb_led4
+runJs buzzer
+runJs rgb_led5
